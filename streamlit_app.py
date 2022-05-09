f31f90984630489079635d1b7450f52ddafb9acc
Pravinkumaar P <66313464+PravinkumaarPonnusamy@users.noreply.github.com>
2022-05-09 16:50:28 +0530

    Update streamlit_app.py
     Fruit Name Column as the Index - choose fruits based on their name

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 My_Fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(My_Fruit_list)
+#streamlit.dataframe(My_Fruit_list)
+My_Fruit_list = My_Fruit_list.set_index('Fruit')
 
 
 # let's put a picklist here so they pick the fruit what they want to include
 streamlit.multiselect("Pick some fruits",list(My_Fruit_list.index))
 
 # display the table on the page
-streamlit.Dataframe(My_Fruit_list)
+streamlit.dataframe(My_Fruit_list)
